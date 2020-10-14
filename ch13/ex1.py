040b48e59c4507c721f4a8b40bb606783238a871
owari-taro <58625526+owari-taro@users.noreply.github.com>
2020-10-14 20:42:39 +0900

    writers causing sstarvation

---
+++ ch13/ex1.py
@@ -0,0 +1,44 @@
+import threading
+import time
+
+
+def writer():
+    global text
+
+    while True:
+        with resource:
+            time.sleep(1.3)
+            print(f'Writing being done by {threading.current_thread().name}.')
+            text += f'Writing was done by {threading.current_thread().name}. '
+
+
+def reader():
+    global rcount
+
+    while True:
+        with rcounter:
+            rcount += 1
+            if rcount == 1:
+                resource.acquire()
+
+        print(f'{rcount=}:Reading being done by {threading.current_thread().name}:')
+        print(text)
+
+        with rcounter:
+            rcount -= 1
+            if rcount == 0:
+                resource.release()
+
+
+if __name__ == "__main__":
+    text = 'This is some text. '
+    rcount = 0
+
+    rcounter = threading.Lock()
+    resource = threading.Lock()
+
+    threads = [threading.Thread(target=reader) for i in range(
+        3)] + [threading.Thread(target=writer) for i in range(2)]
+
+    for thread in threads:
+        thread.start()
